85a03b602fd2f02747bbd4d1ef7359aafb54579e
Manish <manish@gmail.com>
2026-02-08 20:41:49 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def duplicate_logic_1(x):
+    if x > 10:
+        return x * 2
+    else:
+        return x * 2
+
+def duplicate_logic_2(x):
+    if x > 10:
+        return x * 2
+    else:
+        return x * 2
+
